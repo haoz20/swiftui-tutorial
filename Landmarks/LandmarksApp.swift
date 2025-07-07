ab814b50ab280fc0c1d939679aa9efa450353167
haoz20 <zhaonianhao2005@gmail.com>
2025-07-07 22:48:28 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Swan Htet Aung on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
